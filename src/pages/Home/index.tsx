d16397d4639956aad93a77345f0a0eeb4ce3ae48
FranciscoMalstim <franciscosantos2501@gmail.com>
2022-08-24 18:18:12 -0300

    Creating the Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import api from "../../service/api"
 
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: `${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+      console.log(response.data.results)
+    }
+    loadFilmes();
+
+  }, [])
+  
+
   return (
     <h1>Home</h1>
   )
